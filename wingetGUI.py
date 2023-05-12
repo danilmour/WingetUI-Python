4e62a93ca33b1ca67c90abe651a2039a567de869
danilmour <81158510+danilmour@users.noreply.github.com>
2023-05-12 19:55:34 +0100

    Fixed some bugs and refactored the code

--- wingetGUI.py
+++ wingetGUI.py
@@ -1,62 +1,67 @@
 import os
 
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
 
-sg.theme("systemdefault")
+Sg.theme("systemdefault")
 
 layout = [
-    [sg.Text("Instalar Software", font=("Helvetica", 15))],
-    [sg.Button("Instalar Google Chrome"), sg.Button("Instalar Visual Studio Code"), sg.Button("Instalar Adobe Acrobat Reader"), sg.Button("Instalar Virtualbox"), sg.Button("Instalar VLC")],
-    [sg.Text("Instalar Outro Software", font=("Helvetica", 15))],
-    [sg.Input(key="-PACOTE-"), sg.Button("Instalar")],
-    [sg.Text("Atualizações", font=("Helvetica", 15))],
-    [sg.Button("Procurar Atualizações"), sg.Button("Instalar Atualizações")],
-    [sg.Text("", key="-MENSAGEM-")]
+    [Sg.Text("Instalar Software", font=("Helvetica", 15))],
+    [Sg.Button("Instalar Google Chrome"),
+     Sg.Button("Instalar Visual Studio Code"),
+     Sg.Button("Instalar Adobe Acrobat Reader"),
+     Sg.Button("Instalar Virtualbox"),
+     Sg.Button("Instalar VLC")],
+    [Sg.Text("Instalar Outro Software", font=("Helvetica", 15))],
+    [Sg.Input(key="-PACOTE-"), Sg.Button("Instalar")],
+    [Sg.Text("Atualizações", font=("Helvetica", 15))],
+    [Sg.Button("Procurar Atualizações"), Sg.Button("Instalar Atualizações")],
+    [Sg.Text("", key="-MENSAGEM-")]
 ]
 
-window = sg.Window("Winget GUI", layout)
+window = Sg.Window("Winget GUI", layout)
 
-def validarCodigo(codigo, sucesso, falha):
+
+def validar_codigo(codigo, sucesso, falha):
     if codigo == 0:
         window["-MENSAGEM-"].update(sucesso)
     else:
         window["-MENSAGEM-"].update(falha)
 
 
-def installApp(aplicacao):
-    code = os.system(f"winget install {aplicacao} --accept-pakage-agreements")
-    validarCodigo(code, "Instalado com sucesso!", "Ocorreu um erro.")
+def install_app(aplicacao):
+    retorno = os.system(f"winget install {aplicacao}")
+    validar_codigo(retorno, "Instalado com sucesso!", "Ocorreu um erro.")
 
 
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    if event == Sg.WIN_CLOSED:
         break
 
     if event == "Procurar Atualizações":
         code = os.system("winget upgrade")
-        validarCodigo(code, "Não há Atualizações Pendentes.", "Ocorreu um erro.")
+        validar_codigo(code, "Não há Atualizações Pendentes.", "Ocorreu um erro.")
 
     if event == "Instalar Atualizações":
         code = os.system("winget upgrade --all")
-        validarCodigo(code, "Atualizações Instaladas com Sucesso!", "Ocorreu um erro.")
+        validar_codigo(code, "Atualizações Instaladas com Sucesso!", "Ocorreu um erro.")
 
     if event == "Instalar Google Chrome":
-        installApp("google.chrome")
+        install_app("google.chrome")
 
     if event == "Instalar Visual Studio Code":
-        installApp("vscode")
+        install_app("vscode")
 
     if event == "Instalar Adobe Acrobat Reader":
-        installApp("XPDP273C0XHQH2")
+        install_app("XPDP273C0XHQH2")
 
     if event == "Instalar Virtualbox":
-        installApp("virtualbox")
+        install_app("virtualbox")
 
     if event == "Instalar VLC":
-        installApp("VLC media player")
+        install_app("\"VLC media player\"")
 
     if event == "Instalar":
-        installApp(values["-PACOTE-"])
+        install_app(values["-PACOTE-"])
 
 window.close()
